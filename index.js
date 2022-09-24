5c87d5655944daed7ad04143d8ca44244f29b231
OptimisticTrousers <tony.isern@yahoo.com>
2022-09-24 16:40:50 -0400

    Creating a basic http server that is running on port 8080

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const http = require("http");
 const path = require("path");
 const fs = require("fs");
+
+const server = http.createServer(function (req, res) {
+});
+const PORT = 8080;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
