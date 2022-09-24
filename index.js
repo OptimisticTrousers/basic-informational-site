45b39ca651e927f770afed1efac7be63a188b0b6
OptimisticTrousers <tony.isern@yahoo.com>
2022-09-24 16:41:48 -0400

    Writing server-side logic

--- index.js
+++ index.js
@@ -14,6 +14,22 @@ const server = http.createServer(function (req, res) {
 
   let contentType = "text/html";
 
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === "ENOENT") {
+        fs.readFile(path.join(__dirname, "404.html"), (err, content) => {
+          res.writeHead(200, { "Content-Type": contentType });
+          res.end(content, "utf8");
+        });
+      } else {
+        res.writeHead(500);
+        res.end(`Server Error: ${err.code}`);
+      }
+    } else {
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content, "utf8");
+    }
+  });
 });
 const PORT = 8080;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
