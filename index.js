5d38af46c9ff508e06224d513c483da56a0beeb9
OptimisticTrousers <tony.isern@yahoo.com>
2022-09-24 16:41:39 -0400

    Writing code for file paths and names as well as setting the content type as text/html

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const path = require("path");
 const fs = require("fs");
 
 const server = http.createServer(function (req, res) {
+  // const filePath = path.join(__dirname, "public")
+
+  const filePath = path.join(
+    __dirname,
+    req.url === "/" ? "index.html" : req.url
+  );
+
+  let extname = path.extname(filePath);
+
+  let contentType = "text/html";
+
 });
 const PORT = 8080;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
