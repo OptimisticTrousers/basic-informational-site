66a9c9e1b8ccd946e720e41316610d4e1c9dcb7d
OptimisticTrousers <tony.isern@yahoo.com>
2022-10-02 08:22:52 -0400

    Handling routes

--- index.js
+++ index.js
@@ -3,12 +3,51 @@ const fs = require("fs");
 const path = require("path");
 const app = express();
 const PORT = 3000;
+const contentType = "text/html";
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  const filePath = path.join(__dirname, "index.html");
 
-app.get("/about", (req, res) => {});
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === "ENOENT") {
+        fs.readFile(
+          path.join(__dirname, "404.html", (err, content) => {
+            if (err) throw err;
+            res.writeHead({ "Content-Type": contentType });
+            res.end(content, "utf8");
+          })
+        );
+      } else {
+        res.writeHead(500);
+        res.end(content, "utf8");
+      }
+    } else {
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content, "utf8");
+    }
+  });
+});
 
-app.get("/contact", (req, res) => {});
+app.get("/about", (req, res) => {
+  const filePath = path.join(__dirname, "about.html");
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) throw err;
+    res.writeHead(200, { "Content-Type": contentType });
+    res.end(content, "utf8");
+  });
+});
+
+app.get("/contact", (req, res) => {
+  const filePath = path.join(__dirname, "contact.html");
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) throw err;
+    res.writeHead(200, { "Content-Type": contentType });
+    res.end(content, "utf8");
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Express rewrite on port ${port}`);
