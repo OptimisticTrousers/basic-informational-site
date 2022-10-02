1f1ee8562c576a28af81dfa67405d98dbb05069f
OptimisticTrousers <tony.isern@yahoo.com>
2022-10-02 08:39:17 -0400

    Finishing express rewrite

--- index.js
+++ index.js
@@ -9,27 +9,13 @@ app.get("/", (req, res) => {
   const filePath = path.join(__dirname, "index.html");
 
   fs.readFile(filePath, (err, content) => {
-    if (err) {
-      if (err.code === "ENOENT") {
-        fs.readFile(
-          path.join(__dirname, "404.html", (err, content) => {
-            if (err) throw err;
-            res.writeHead({ "Content-Type": contentType });
-            res.end(content, "utf8");
-          })
-        );
-      } else {
-        res.writeHead(500);
-        res.end(content, "utf8");
-      }
-    } else {
-      res.writeHead(200, { "Content-Type": contentType });
-      res.end(content, "utf8");
-    }
+    if (err) throw err;
+    res.writeHead(200, { "Content-Type": contentType });
+    res.end(content, "utf8");
   });
 });
 
-app.get("/about", (req, res) => {
+app.get("/about.html", (req, res) => {
   const filePath = path.join(__dirname, "about.html");
 
   fs.readFile(filePath, (err, content) => {
@@ -39,8 +25,8 @@ app.get("/about", (req, res) => {
   });
 });
 
-app.get("/contact", (req, res) => {
-  const filePath = path.join(__dirname, "contact.html");
+app.get("/contact-me.html", (req, res) => {
+  const filePath = path.join(__dirname, "contact-me.html");
 
   fs.readFile(filePath, (err, content) => {
     if (err) throw err;
@@ -49,6 +35,16 @@ app.get("/contact", (req, res) => {
   });
 });
 
+app.all("*", async (req, res) => {
+  const filePath = path.join(__dirname, "404.html");
+  try {
+    const data = await fs.promises.readFile(filePath, "utf8");
+    res.status(404).send(data);
+  } catch (err) {
+    throw err;
+  }
+});
+
 app.listen(PORT, () => {
-  console.log(`Express rewrite on port ${port}`);
+  console.log(`Express rewrite on port ${PORT}`);
 });
